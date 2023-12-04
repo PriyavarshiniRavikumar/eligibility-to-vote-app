a8f091a8991c89be8b42b2c2917124adf51cd82a
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-12-04 22:23:41 +0530

    querySelector

--- app/script.js
+++ app/script.js
@@ -1,1 +1,5 @@
 // JavaScript source code
+const ageInput = document.querySelector("#age");
+const checkbtn = document.querySelector("#check");
+const clearbtn = document.querySelector("#clear");
+const result = document.querySelector("#result");
