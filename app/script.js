6d30e7351c235fa73a4a695c0a92b1172aa3ea5a
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-12-04 22:24:47 +0530

    comments

--- app/script.js
+++ app/script.js
@@ -3,3 +3,14 @@ const ageInput = document.querySelector("#age");
 const checkbtn = document.querySelector("#check");
 const clearbtn = document.querySelector("#clear");
 const result = document.querySelector("#result");
+/*function printToResult(text) {
+    if (text) {
+        resultDiv.innerHTML = '<h3>'+text+'</h3>'
+    }
+
+    // if (!text) {
+    //     return;
+    // }
+    // resultDiv.innerHTML = '<h3>'+text+'</h3>'
+
+}
