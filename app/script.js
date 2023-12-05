75a0d6d91c943ec57cfee4f22595594ce76033c5
PriyavarshiniRavikumar <varsharkg1@gmail.com>
2023-12-05 22:10:21 +0530

    comments

--- app/script.js
+++ app/script.js
@@ -14,3 +14,28 @@ const result = document.querySelector("#result");
     // resultDiv.innerHTML = '<h3>'+text+'</h3>'
 
 }
+// calling a function when the button gets clicked
+calculateBtn.addEventListener('click', function() {
+    let result = '';
+
+    const userAge = ageInputBox.value;
+    if (!userAge) {
+        printToResult('Please enter an age')
+        return;
+    }
+
+
+    if (userAge < 18) {
+        console.log('age is ', userAge)
+        console.log('You are not eligible to vote')
+        // resultDiv.innerHTML = '<h3>You are not eligible to vote</h3>'
+        result = 'You are not eligible to vote'
+    } else {
+        console.log('You are eligible to vote')
+        // resultDiv.innerHTML = '<h3>You are eligible to vote</h3>'
+        result = 'You are eligible to vote'
+    }
+
+    // concatenation
+    printToResult(result)
+})*/
